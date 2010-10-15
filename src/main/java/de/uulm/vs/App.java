a624eafa20828d5867eb41eeaa71dce15d5d2f7d
Sven Schober <sven.schober@uni-ulm.de>
2010-10-15 11:00:54 +0200

    inital commit
    A netty example using maven

---
+++ src/main/java/de/uulm/vs/App.java
@@ -0,0 +1,13 @@
+package de.uulm.vs;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
